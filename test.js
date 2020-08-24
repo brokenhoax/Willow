da84d3e8bbd51ed43148fddd0676289f879bd411
brokenhoax <brokenhoax@outlook.com>
2020-08-23 22:28:50 -0500

    Reorganizing Files for Github Pages Functionality

---
+++ test.js
@@ -0,0 +1,15 @@
+
+    const color = ['blue', 'red', 'green', 'red', 'purple', 'red']
+
+    const uniqueCategories = [...new Set(color)]
+
+    console.log(uniqueCategories)
+
+    // for (var i = 0; i <= uniqueCategories.length; i++) {
+
+    //   console.log(i)
+
+    //   console.log(color)
+    // }
+
+        
